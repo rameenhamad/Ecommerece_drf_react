d12d70214a8ac56cc096a2a6bd0250ede0ebaed2
rameenhamad <rameenhammadahmad@gmail.com>
2026-06-23 19:13:52 +0500

    Serve media files via custom view (works without DEBUG)

--- ecommerce_backend/ecommerce_backend/urls.py
+++ ecommerce_backend/ecommerce_backend/urls.py
@@ -1,23 +1,9 @@
-"""
-URL configuration for ecommerce_backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+import os
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.http import FileResponse, HttpResponseNotFound
+from django.views.decorators.http import require_GET
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +12,13 @@ urlpatterns = [
     path('api/user/', include('users.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+@require_GET
+def serve_media(request, path):
+    file_path = os.path.join(settings.MEDIA_ROOT, path)
+    if os.path.exists(file_path):
+        return FileResponse(open(file_path, 'rb'))
+    return HttpResponseNotFound()
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve_media),
+]

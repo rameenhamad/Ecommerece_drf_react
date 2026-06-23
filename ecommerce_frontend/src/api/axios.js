9e663aa8f8ea927bcc59f4625e185175a86c21a2
rameenhamad <rameenhammadahmad@gmail.com>
2026-06-23 16:31:57 +0500

    Fix VITE_BACKEND_URL -> derive from VITE_API_URL

--- ecommerce_frontend/src/api/axios.js
+++ ecommerce_frontend/src/api/axios.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 const API_BASE = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api";
 
-const BACKEND_URL =
-  import.meta.env.production.VITE_BACKEND_URL || "http://127.0.0.1:8000";
+const BACKEND_URL = import.meta.env.VITE_API_URL
+  ? import.meta.env.VITE_API_URL.replace(/\/api\/?$/, "")
+  : "http://127.0.0.1:8000";
 
 const api = axios.create({
   baseURL: API_BASE,

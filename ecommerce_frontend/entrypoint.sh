bf049c458ac45381a818f67c119743a5d8941e9e
rameenhamad <rameenhammadahmad@gmail.com>
2026-06-23 17:46:30 +0500

    Also replace placeholder in .html files (index.html)

--- ecommerce_frontend/entrypoint.sh
+++ ecommerce_frontend/entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 
 API_URL="${VITE_API_URL:-http://localhost:8000/api}"
 
-find /usr/share/nginx/html -type f -name "*.js" -exec sed -i "s|__VITE_API_URL__|$API_URL|g" {} +
+find /usr/share/nginx/html -type f \( -name "*.js" -o -name "*.html" \) -exec sed -i "s|__VITE_API_URL__|$API_URL|g" {} +
 
 exec nginx -g "daemon off;"
